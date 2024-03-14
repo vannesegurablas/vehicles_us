5c30ae7b94cff72b993ea3ad265b74d1350bb666
vannesegurablas <162353780+vannesegurablas@users.noreply.github.com>
2024-03-14 12:23:56 -0600

    primer commit

--- app.py
+++ app.py
@@ -2,19 +2,24 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st
 
-st.header('Creación de gráficas para los vehiculos en US')      
+st.header('Creación de gráficas para los vehiculos en US')
+st.header('Aca se muestran los valores del DF')
+
+car_data = pd.read_csv('vehicles_us.csv') # leer los datos
+st.dataframe(car_data)
 
 car_data = pd.read_csv('vehicles_us.csv') # leer los datos
 build_histogram = st.checkbox('Quieres construir un histograma según el modelo de los coches?')
 hist_button = st.button('Construir histograma') # crear un botón
-        
+
 if hist_button: # al hacer clic en el botón
     st.write('Creación de un histograma para el conjunto de datos segun el modelo de los coches')
             
     fig = px.histogram(car_data, x="model_year")
         
     st.plotly_chart(fig, use_container_width=True)
-
+else:
+    st.write('Siguiente')
 
 build_dispersion= st.checkbox('Quieres construir un diagrama de dispersión según el odométro?')
 disp_button = st.button('Construir diagrama de dispersión') # crear un botón
@@ -25,6 +30,8 @@ if disp_button: # al hacer clic en el botón
     fig = px.scatter(car_data, x="odometer", y="price")
         
     st.plotly_chart(fig, use_container_width=True)
+else:
+    st.write('Siguiente')
 
 build_pie= st.checkbox('Quieres saber las condiciones del coche según el modelo mediante un gráfico circular?')
 pie_button = st.button('Construir gráfico circular')
@@ -35,6 +42,8 @@ if pie_button: # al hacer clic en el botón
     fig = px.pie(car_data, values='model_year', names='condition')
         
     st.plotly_chart(fig, use_container_width=True)
+else:
+    st.write('Siguiente')
 
 build_sun= st.checkbox('Quieres saber las condiciones del coche según el modelo mediante un gráfico sunburst?')
 sun_button = st.button('Construir gráfico sunburst')
@@ -48,3 +57,7 @@ if sun_button: # al hacer clic en el botón
         values='cylinders',
     )
     st.plotly_chart(fig, use_container_width=True)
+else:
+    st.write('Deberias verlo')
+
+st.header('!Goodbye :)!')
