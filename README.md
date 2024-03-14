ec8dca8ba22070247e6f3ff73e2477691f851191
vannesegurablas <162353780+vannesegurablas@users.noreply.github.com>
2024-03-14 11:09:01 -0600

    primer commit

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # vehicles_us
 Cree el primer commit en donde se puede crear un histograma y me preguntan si quiero realmente hacerlo y lo confirmo todo ocurre en el siguiente link http://localhost:8501/
+Olvide colocar el encabezado aca lo agege http://localhost:8501/
+Cambie el tipo de histrogram que quiero mostrar y selecciono el modelo_year queda asi http://localhost:8501/
+Al colocar todos las graficas que quiero me salio este link http://localhost:8501/
+
+
--- app.py
+++ app.py
@@ -5,12 +5,33 @@ import streamlit as st
 st.header('Creación de gráficas para los vehiculos en US')      
 
 car_data = pd.read_csv('vehicles_us.csv') # leer los datos
-build_histogram = st.checkbox('Quieres construir un histograma')
+build_histogram = st.checkbox('Quieres construir un histograma según el modelo de los coches?')
 hist_button = st.button('Construir histograma') # crear un botón
         
 if hist_button: # al hacer clic en el botón
-    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+    st.write('Creación de un histograma para el conjunto de datos segun el modelo de los coches')
+            
+    fig = px.histogram(car_data, x="model_year")
+        
+    st.plotly_chart(fig, use_container_width=True)
+
+
+build_dispersion= st.checkbox('Quieres construir un diagrama de dispersión según el odométro?')
+disp_button = st.button('Construir diagrama de dispersión') # crear un botón
+
+if disp_button: # al hacer clic en el botón
+    st.write('Creación de un diagrama de dispersión según el odométro de los coches')
             
     fig = px.histogram(car_data, x="odometer")
+        
+    st.plotly_chart(fig, use_container_width=True)
+
+build_pie= st.checkbox('Quieres saber las condiciones del coche según el modelo mediante un gráfico circular?')
+pie_button = st.button('Construir gráfico circular')
+
+if pie_button: # al hacer clic en el botón
+    st.write('Creación de un gráfico circular según las condicones los coches')
+            
+    fig = px.pie(car_data, values='model_year', names='condition')
         
     st.plotly_chart(fig, use_container_width=True)
